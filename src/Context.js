1f2552875c7b39a3a39d06b9a7075ab35cfc00c4
Ashni Shah <ashni@uw.edu>
2021-02-25 16:15:32 -0800

    context.js but we have no context

---
+++ src/Context.js
@@ -0,0 +1,71 @@
+import React, { useState, createContext, useEffect } from "react";
+import { firebase, fireauth, firestore } from "./firebase";
+
+const Context = createContext();
+
+const ContextProvider = props => {
+    const [ user, setUser ] = useState();
+
+    useEffect(() => {
+        let unsubscribe = () => {};
+
+        fireauth.onAuthStateChanged(async localUserData => {
+            unsubscribe();
+            if (localUserData) {
+                const userRef = firestore.collection("users").doc(localUserData.uid);
+
+                (await userRef.get()).exists || await userRef.set({
+                    displayName: localUserData.displayName,
+                    email: localUserData.email,
+                    timeCreated: firebase.firestore.FieldValue.serverTimestamp(),
+                    admin: false,
+                    points: 0,
+                });
+                unsubscribe = userRef.onSnapshot(snapshot => {
+                    setUser({
+                        ...snapshot.data(),
+                        local: { ...localUserData },
+                        level: computeLevel(snapshot.data().points)
+                    });
+                });
+            } else {
+                unsubscribe = () => {};
+                setUser(localUserData);
+            }
+        });
+
+        return unsubscribe;
+    }, []);
+
+    useEffect(() => {
+        const updatePoints = async () => {
+            if (user) {
+                const userRef = firestore.collection("users").doc(user.local.uid);
+                const unsubscribe = firestore.collection("posts")
+                    .where("user", "==", userRef)
+                    .onSnapshot(snapshot => {
+                        let newPoints = 0;
+                        if (!snapshot.empty) {
+                            snapshot.forEach(post => {
+                                post = post.data();
+                                newPoints += (post.likes.length + 5);
+                            });
+                        }
+
+                        userRef.update({ points: newPoints });
+                    });
+
+                return unsubscribe;
+            }
+        }
+
+        updatePoints();
+    }, [ user ]);
+
+
+    return <Context.Provider value={{user}}>
+        {props.children}
+    </Context.Provider>;
+};
+
+export { Context, ContextProvider };
