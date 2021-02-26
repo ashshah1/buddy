3add8f52e00f178899a37a17e664ab64ababcc56
Kayla Chea <kaylamchea@hotmail.com>
2021-02-25 18:00:23 -0800

    Fixed task css

--- src/Task.js
+++ src/Task.js
@@ -4,23 +4,25 @@ import React from 'react';
 import './Task.css';
 
 function Task(props) {
+    let currProgress = (props.currCount/props.taskFreq) * 100 + "%";
     return (<div className="task-container">
-        <div className="curr-progress" style={{width: "20%", height:"5rem"}}>
+        <div className="curr-progress" style={{width: currProgress}}>
             &nbsp;
         </div>
-        <div className="container-one">
-            <p id="dot-dot-dot">dot</p>
-            <p>{props.taskName}</p>
-        </div>
-        <div className="container-two">
-            <p>{props.taskDuration}</p>
-            <p>{props.taskFreq}</p>
-            <p>{props.currCount}</p>
-            <p>{props.totalCount}</p>
+        <div id="padding-div">
+            <div className="container-one">
+                <p id="dot-dot-dot">dot</p>
+                <p>{props.taskName}</p>
+            </div>
+            <div className="container-two">
+                <p>
+                    <a>{props.taskDuration}  &nbsp;</a>
+                    <a><a>{props.currCount} {" / "}</a><a>{props.taskFreq}</a></a>
+                </p>
+                <p>{props.totalCount}</p>
+            </div>
         </div>
     </div>);
  }
 
 export default Task;
-
-
