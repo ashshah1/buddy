48f3c65ad37e9394dd9b1d4b3bdde34d3a695ddc
Kayla Chea <kaylamchea@hotmail.com>
2021-03-05 15:27:09 -0800

    Fixed landing page css

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,7 @@
 import { Context } from "./Context";
 import React, { useState, useContext } from "react";
 import { firebase, fireauth } from "./firebase";
-import background from "./backgrounds/bg-one.png";
+import background from "./images/bg-one.png";
 
 import TaskList from "./TaskList.js";
 import LogInPage from "./LogInPage.js";
