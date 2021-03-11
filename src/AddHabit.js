ca3a55544122b1b44c3382a1a789949b9c814378
Ash Shah <ashni@uw.edu>
2021-03-10 17:19:02 -0800

    finish add habit functionality, css needs work

--- src/AddHabit.js
+++ src/AddHabit.js
@@ -10,10 +10,10 @@ function AddHabit() {
     // keep track of all the values being added to the new habit
     const [name, setName] = useState("");
     const [color, setColor] = useState("");
-    const [category, setCategory] = useState("");
-    const [frequency, setFrequency] = useState("");
-    const [counter, setCounter] = useState(0);
+    const [category, setCategory] = useState("mind");
+    const [frequency, setFrequency] = useState(0);
     const [state, setState] = useState("SELECT");
+    const [repeat, setRepeat] = useState("");
 
 
     const colors = ["#A9E3EB", "#69AABF", "#C1F8E4", "#ACECB7", "#F5BF32", "#F98A5B", "#C1C6F8", "#C4C4C4"]
@@ -28,16 +28,22 @@ function AddHabit() {
     const resetModal = () => {
         setName("");
         setState("");
+        setColor("");
+        setCategory("mind");
+        setFrequency(0);
     }
 
+    
+
     useEffect(() => {
+
         // creates a new habit object which is then added to the firestore
         const createHabit = async () => {
             const newHabitObj = {
-                category: "Mind",
+                category: category,
                 color: color,
                 currCounter: 0,
-                daily: true, // todo: update somehow with the buttons
+                daily: (repeat === "daily"), // todo: update somehow with the buttons
                 exp: 10,
                 frequency: frequency,
                 name: name,
@@ -45,7 +51,7 @@ function AddHabit() {
                 overallCounter: 0,
                 timestamp: firebase.firestore.FieldValue.serverTimestamp(),
                 user: firestore.collection("users").doc(user.local.uid),
-                weekly: false
+                weekly: (repeat === "weekly")
             };
             await firestore.collection("Habits").add(newHabitObj)
         };
@@ -57,6 +63,12 @@ function AddHabit() {
 
     // on click mark button as active, disable others?
     // somehow save the text value of that button for both frequency and category and set their states
+
+
+
+    const handleCategory = (event) => {
+        setCategory(event.target.innerText);
+    }
 
 
 
@@ -74,8 +86,8 @@ function AddHabit() {
                     <div className="form-group pb-2">
                         <label>Repeat</label>
                         <div className="frequency">
-                            <button className="daily-btn">daily</button>
-                            <button className="weekly-btn">weekly</button>
+                            <button onClick={event => {setRepeat("daily")}} className="daily-btn" type="button">daily</button>
+                            <button onClick={event => {setRepeat("weekly")}} className="weekly-btn" type="button">weekly</button>
                         </div>
                     </div>
                     <div className="form-group pb-2">
@@ -88,19 +100,15 @@ function AddHabit() {
                     <div className="form-group pb-2">
                         <label>Select a category</label>
                         <div className="category-type">
-                            <button className="filled">mind</button>
-                            <button className="not-filled">body</button>
-                            <button className="not-filled">life</button>
-                            <button className="not-filled">other</button>
+                            <button onClick={handleCategory} className="not-filled" type="button">mind</button>
+                            <button onClick={handleCategory} className="not-filled" type="button">body</button>
+                            <button onClick={handleCategory} className="not-filled" type="button">life</button>
+                            <button onClick={handleCategory} className="not-filled" type="button">other</button>
                         </div>
                     </div>
                     <div className="form-group pb-2">
                         <label>Select a color</label>
                         <div>
-                            {/* <button className="color-btn no-font m-4" onClick={event => setColor(event.target.innerText)} type="button">#F56D32</button>
-                            <button className="color-btn no-font m-4" onClick={event => setColor(event.target.innerText)} type="button">#C1F8E4</button>
-                            <button className="color-btn no-font m-4" onClick={event => setColor(event.target.innerText)} type="button">#F5BF32</button>
-                            <button className="color-btn no-font m-4" onClick={event => setColor(event.target.innerText)} type="button">#69AABF</button> */}
                              {colors.map(circleColor => (
                                 <button className="color-btn no-font m-4" onClick={event => setColor(event.target.innerText)} style={{backgroundColor : circleColor}} type="button">{circleColor}</button>
                             ))}
@@ -115,6 +123,33 @@ function AddHabit() {
         </div>
 
     )
+}
+
+
+function BuddyButtons(props) {
+    const [buttonFill, setButtonFill] = useState(false);
+
+    let buttonstyle = "";
+    
+
+    const handleCategory = () => {
+        setButtonFill(!buttonFill);
+    }
+     
+    if (buttonFill) {
+        buttonstyle = "filled";
+    } else {
+        buttonstyle = "not-filled"
+    }
+
+    // send props of w
+    return (
+        <button onClick={handleCategory} className={buttonstyle} type="button">{props.category}</button>
+        /* <button className="not-filled" type="button">body</button>
+        <button className="not-filled" type="button">life</button>
+        <button className="not-filled" type="button">other</button> */
+    )
+
 }
 
 export default AddHabit;
